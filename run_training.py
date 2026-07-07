4484c578d335e7e80c3192422515f3f3e3806460
Xinlong <xliu5974@gmail.com>
2026-07-07 12:35:23 -0500

    updated training function

--- run_training.py
+++ run_training.py
@@ -1,11 +1,14 @@
 import Trainer
 
-layers = ["L1F","L2","L3M","L3P","L4M","L4P"]
+layers = ["L1U", "L1F","L2","L3M","L3P","L4M","L4P"]
 axes = ["x","y"]
 for layer in layers:
     for axis in axes:
         trainer = Trainer.Trainer(layer=layer, axis=axis)
+        #trainer.debug_input()
+        #continue
         trainer.prepare_train_test_input()
         trainer.train()
         trainer.test()
         trainer.visualize()
+        trainer.plot_otherMethods()
